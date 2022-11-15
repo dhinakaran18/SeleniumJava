b5e77e205efdd9f5083543cb15411ffe25d7cf13
Dhinakaran <dhinakarankvp@gmail.com>
2022-11-16 00:40:21 +0530

    Java week 1 day 1 & 2 assignment

---
+++ seleniumjava/src/main/java/week1/day1/Mobile.java
@@ -0,0 +1,35 @@
+package week1.day1;
+
+public class Mobile {
+
+	String mobileBrandName = "Redmi Note 9";
+	char mobileLogo = 'M';
+	short noOfMobilePiece = 2;
+	int modelNumber = 9;
+	long mobileImeiNumber = 7876564238756l;
+	long mobileImeiNumber1 = 7876564238756l;
+	float mobilePrice = 6999.99f;
+	boolean isDamaged = false;
+
+	public static void main(String[] args) {
+		Mobile m = new Mobile();
+
+//		String brandname = m.mobileBrandName;
+//		char logo = m.mobileLogo;
+//		short piece = m.noOfMobilePiece;
+//		int number = m.modelNumber;
+//		long imei = m.mobileImeiNumber;
+//		long imei1 = m.mobileImeiNumber1;
+//		float price = m.mobilePrice; 
+//		boolean isDamaged = m.isDamaged;
+
+		System.out.println("The Mobile Brand is " + m.mobileBrandName);
+		System.out.println("The Logo is " + m.mobileLogo);
+		System.out.println("The No of Mobile pieces in stock is " + m.noOfMobilePiece);
+		System.out.println("Mobile Model Number is " + m.modelNumber);
+		System.out.println("Mobile IMEI Number is '" + m.mobileImeiNumber + "' & '" + m.mobileImeiNumber1 + "'");
+		System.out.println("The Mobile Prize is " + m.mobilePrice);
+		System.out.println("Is that Mobile damaged? " + m.isDamaged);
+	}
+
+}
---
+++ seleniumjava/src/main/java/week1/day2/ConvertNegativeNumberToPositiveNumber.java
@@ -0,0 +1,16 @@
+package week1.day2;
+
+public class ConvertNegativeNumberToPositiveNumber {
+	
+	public static void main(String[] args) {
+		int number = -40;
+		
+		if (number < 0)
+		{
+			int num = number * -1;
+			System.out.println(num);
+		}
+
+	}
+
+}
